d486f4fcd34af0deeb40385aa9123cf92a63a880
TetiPalii <TetianaPalii9491@gmail.com>
2023-04-10 22:53:18 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,11 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const submitButton = document.querySelector("[type ='submit']");
+// const submitButton = document.querySelector("[type ='submit']");
 const emailInput = document.querySelector("[name='email']");
 const messageEl = document.querySelector('[name="message"]');
 const STORAGE_KEY = 'feedback-form-state';
 const allValues = {};
-
-setInputValues();
-
-form.addEventListener('input', onInput);
-function onInput(e) {
-  allValues.email = e.currentTarget.elements.email.value;
-  allValues.message = e.currentTarget.elements.message.value;
-
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(allValues));
-
-  form.addEventListener('click', onSubmitButton);
-  function onSubmitButton() {
-    console.log(allValues);
-    localStorage.clear();
-  }
-}
 
 function setInputValues() {
   const savedValues = localStorage.getItem(STORAGE_KEY);
@@ -32,18 +16,21 @@ function setInputValues() {
   }
 }
 
-// submitButton.addEventListener('click', onSubmitButton);
-// function onSubmitButton(e) {
-//   localStorage.clear();
-//   console.log(localStorage.getItem(STORAGE_KEY));
-// }
+setInputValues();
 
-// const savedValues = localStorage.getItem(JSON.parse(STORAGE_KEY));
-// e.currentTarget.elements.message.value = obj.message;
-// e.currentTarget.elements.email.value = obj.email;
-// console.log(savedValues);
+form.addEventListener('input', onInput);
+function onInput(e) {
+  allValues.email = e.currentTarget.elements.email.value;
+  allValues.message = e.currentTarget.elements.message.value;
 
-// function setInputValues(obj) {
-//   e.currentTarget.elements.message.value = obj.message;
-//   e.currentTarget.elements.email.value = obj.email;
-// }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(allValues));
+}
+
+form.addEventListener('submit', throttle(onSubmit, 500));
+
+function onSubmit(e) {
+  e.preventDefault();
+  localStorage.clear();
+  console.log(allValues);
+  form.reset();
+}
