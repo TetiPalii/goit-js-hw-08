af5273eb258ed0c96cf100807a1c672094e8930f
TetiPalii <TetianaPalii9491@gmail.com>
2023-04-16 21:47:52 +0200

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,6 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-// const submitButton = document.querySelector("[type ='submit']");
 const emailInput = document.querySelector("[name='email']");
 const messageEl = document.querySelector('[name="message"]');
 const STORAGE_KEY = 'feedback-form-state';
@@ -34,3 +33,48 @@ function onSubmit(e) {
   console.log(allValues);
   form.reset();
 }
+
+// import throttle from 'lodash.throttle';
+// const FEEDBACK_FS = 'feedback-form-state';
+// let userData = {};
+// const feedbackForm = document.querySelector('.feedback-form');
+// const input = document.querySelector('input');
+// const textarea = document.querySelector('textarea');
+
+// feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+// feedbackForm.addEventListener('submit', onFormSubmit);
+
+// fillInputs();
+
+// function onFormInput(e) {
+//   // console.dir(e.target.name);
+//   userData[e.target.name] = e.target.value;
+//   // console.log(userData[e.target.name]);
+//   localStorage.setItem(FEEDBACK_FS, JSON.stringify(userData));
+// }
+
+// function onInput(e) {
+//   //   allValues.email = e.currentTarget.elements.email.value;
+//   //   allValues.message = e.currentTarget.elements.message.value;
+
+//   //   localStorage.setItem(STORAGE_KEY, JSON.stringify(allValues));
+//   // }
+// function fillInputs() {
+//   const savedData = JSON.parse(localStorage.getItem(FEEDBACK_FS));
+//   if (savedData) {
+//     input.value = savedData.email;
+//     textarea.value = savedData.message;
+//     userData = savedData;
+//   }
+// }
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   if (input.value === '' || textarea.value === '') {
+//     return;
+//   }
+//   e.currentTarget.reset();
+//   localStorage.removeItem(FEEDBACK_FS);
+//   console.log(userData);
+//   userData = {};
+// }
